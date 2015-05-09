f70824cc1f638f353ea0fac144e309baf69f222f
Marc Duiker <marcduiker@gmail.com>
2015-05-09 23:02:45 +0200

    Added files for Sample MCV 3. Ignored ncrunch files.

---
+++ source/SitecoreSolution/SitecoreSolution.SitecoreLayer/ItemHelper.cs
@@ -0,0 +1,18 @@
+﻿using Sitecore.Data;
+using Sitecore.Data.Items;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SitecoreSolution.SitecoreLayer
+{
+    public static class ItemHelper
+    {
+        public static Item GetItem(ID itemId)
+        {
+            return Sitecore.Context.Database.GetItem(itemId);
+        }
+    }
+}
